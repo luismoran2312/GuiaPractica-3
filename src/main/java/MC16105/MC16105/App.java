c86df331a521478293aa4bb5240355de1c758948
MC16105 <you@example.com>
2024-05-02 18:47:02 -0600

    first commit

---
+++ src/main/java/MC16105/MC16105/App.java
@@ -0,0 +1,14 @@
+package MC16105.MC16105;
+
+public class App 
+{
+	public String Hello () {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+        App hw = new App ();
+        System.out.println(hw.Hello());
+    }
+}
---
+++ src/test/java/MC16105/MC16105/AppTest.java
@@ -0,0 +1,20 @@
+package MC16105.MC16105;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
